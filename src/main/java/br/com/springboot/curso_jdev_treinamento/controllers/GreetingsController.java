3213b1dddfdfeb6c4b5ee9ee101ba68aa7395c88
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-01-15 00:43:37 -0300

    Criação do endpoint para salvar usuario

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -6,11 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.springboot.curso_jdev_treinamento.model.Usuario;
@@ -26,25 +24,14 @@ public class GreetingsController {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
-    /**
-     *
-     * @param name the name to greet
-     * @return greeting text
-     */
-    @RequestMapping(value = "/mostrarnome/{name}/{idade}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String greetingText(@PathVariable String name, @PathVariable int idade) {
-    	
-    	Usuario usuario = new Usuario();
-    	usuario.setNome(name);
-    	usuario.setIdade(idade);
-    	
-    	usuario = usuarioRepository.save(usuario);
-    	
-    	System.out.println(usuario);
-    	
-        return "Iniciando estudos com SpringBoot: " + name + "!";
-    }
+   @ResponseBody
+   @PostMapping(value = "/salvarUsuario")
+   public ResponseEntity<Usuario> salvarUsuario(@RequestBody Usuario usuario){
+	   
+	   usuario = usuarioRepository.saveAndFlush(usuario);
+	   
+	   return new ResponseEntity<Usuario>(usuario, HttpStatus.CREATED);
+   }
     
     @ResponseBody
     @GetMapping(value = "/listarUsuarios")
