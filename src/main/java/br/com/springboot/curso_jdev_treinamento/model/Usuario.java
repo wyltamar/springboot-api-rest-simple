1e2bac1caa471f612951adf55672e035d776a51f
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-01-14 23:37:04 -0300

    Criação da entidade Usuario no banco de dados

---
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/model/Usuario.java
@@ -0,0 +1,68 @@
+package br.com.springboot.curso_jdev_treinamento.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@SequenceGenerator(name = "seq_usuario", allocationSize = 1, initialValue = 1)
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(generator = "seq_usuario", strategy = GenerationType.SEQUENCE)
+	private Long id;
+	
+	private String nome;
+	
+	private int idade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+}
