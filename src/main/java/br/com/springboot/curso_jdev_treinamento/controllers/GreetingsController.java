953a2337953b33a47445a281be6da579f4d233e0
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-01-15 00:27:42 -0300

    Criação de end point para listar todos os usuarios

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,10 +1,15 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +44,14 @@ public class GreetingsController {
     	System.out.println(usuario);
     	
         return "Iniciando estudos com SpringBoot: " + name + "!";
+    }
+    
+    @ResponseBody
+    @GetMapping(value = "/listarUsuarios")
+    public ResponseEntity<List<Usuario>> ListarUsuarios(){
+    	
+    	List<Usuario> usuariosDoBanco = usuarioRepository.findAll();
+    	
+    	return new ResponseEntity<List<Usuario>>(usuariosDoBanco, HttpStatus.OK);
     }
 }
