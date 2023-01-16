613745dbef983657dc7e17522d6cf82cbbfb5f68
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-01-15 21:52:50 -0300

    Criando endpoints para buscar e atualizar o registro no banco de dados

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -53,5 +54,29 @@ public class GreetingsController {
     	return new ResponseEntity<String>("Usuario número: "+idUsuario+" excluído com sucesso",
     			HttpStatus.OK);
     	
+    }
+    
+    @ResponseBody
+    @GetMapping(value = "/buscarUsuarioPorId")
+    public ResponseEntity<?> buscarUsuarioPorId(@RequestParam("id") Long id){
+    	
+    	Usuario usuario = usuarioRepository.findById(id).orElse(null);
+    	
+    	if(usuario == null) return new ResponseEntity<String>("Usuário não encontrado", HttpStatus.NOT_FOUND);;
+    	
+    	return new ResponseEntity<Usuario>(usuario, HttpStatus.FOUND);
+    }
+    
+    @ResponseBody
+    @PutMapping(value = "/atualizar")
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario){
+ 	   
+       if(usuario.getId() == null) {
+    	   return new ResponseEntity<String>("O campo id é obrigatório!",HttpStatus.BAD_REQUEST);
+       }
+    	
+ 	   usuario = usuarioRepository.saveAndFlush(usuario);
+ 	   
+ 	   return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
     }
 }
